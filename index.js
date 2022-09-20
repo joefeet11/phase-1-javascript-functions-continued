6ed822c3e935c53208c36a14f97b9b60c0ca2c09
Kevin Olivier <kevinolivier1111@gmail.com>
2022-09-20 11:58:00 -0600

    commit tree e61bacfb595bb2c1184e02ae2f2cb721a229f52d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+const mondayWork = function (work= "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+mondayWork();
+
+function wrapAdjective(pam1 ="*") {
+    return function(pam2 = "special") {
+        return `You are ${pam1}${pam2}${pam1}!`
+    }
+
+}
+wrapAdjective("%")("a dedicated programmer");
